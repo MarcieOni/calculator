f734a6b49b84b8c57a80f61278e2ead8f31f968e
Emily Onishi <emilyonishi93@gmail.com>
2023-07-05 10:41:20 -0400

    refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,4 @@ function add(number1, number2) {
 
 saySomething("hi");
 
-const result = add(3, 5);
-const outputText = "the sum is " + result + ".";
-saySomething(outputText);
+saySomething("The sum is " + add(3,5) + ".")
