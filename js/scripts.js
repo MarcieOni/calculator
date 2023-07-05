ed080e04ec8d0f45bccb7c535e11affab8ca4856
Emily Onishi <emilyonishi93@gmail.com>
2023-07-05 11:38:59 -0400

    add subtraction function

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,11 @@ function add(number1, number2) {
     return number1 + number2;
 }
 
+function subtract(number1, number2){
+    return number1 - number2;
+}
 //USER INTERFACE LOGIC
 const number1 = parseInt(prompt("enter a number"));
 const number2 = parseInt(prompt ("enter another number"));
 
-window.alert(add(number1, number2));
+window.alert("the sum of ur numbers is " + add(number1, number2) + ". ALSO.. " + number1 + " - " + number2 + " = " + subtract(number1, number2));
