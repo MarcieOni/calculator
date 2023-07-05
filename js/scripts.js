298c69a2404dd0261aeec9c524e6ef1da38575c5
Emily Onishi <emilyonishi93@gmail.com>
2023-07-05 11:52:47 -0400

    add multiply n divide functions

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,17 @@ function add(number1, number2) {
 function subtract(number1, number2){
     return number1 - number2;
 }
+
+function multiply(number1, number2){
+    return number1 * number2
+}
+
+function divide(number1, number2){
+    return number1 / number2
+}
+
 //USER INTERFACE LOGIC
 const number1 = parseInt(prompt("enter a number"));
 const number2 = parseInt(prompt ("enter another number"));
 
-window.alert("the sum of ur numbers is " + add(number1, number2) + ". ALSO.. " + number1 + " - " + number2 + " = " + subtract(number1, number2));
+window.alert("the sum of ur numbers is " + add(number1, number2) + ". ALSO.. " + number1 + " - " + number2 + " = " + subtract(number1, number2) + " | " + number1 + " x " + number2 + " = " + multiply(number1, number2) + " | " + number1 + " ÷ " + number2 + " = " + divide(number1, number2));
